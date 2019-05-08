bc2ddb8e32e258ecc85ab36dd20c63bcbd61ff88
Vincentyun <vincentzyc@qq.com>
2019-05-08 10:55:29 +0800

    根据路由动态设置当前选中菜单栏

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -23,36 +23,37 @@ export default function BasicLayout(props) {
           mode="inline"
           onClick={handleClick}
           defaultSelectedKeys={[props.location.pathname]}
+          selectedKeys={[props.location.pathname]}
         >
           <Menu.Item key="/">
             <Icon type="user" />
             <span className="nav-text">nav 1</span>
           </Menu.Item>
-          <Menu.Item key="/Page2">
+          <Menu.Item key="/page2">
             <Icon type="video-camera" />
             <span className="nav-text">nav 2</span>
           </Menu.Item>
-          <Menu.Item key="/Page3">
+          <Menu.Item key="/page3">
             <Icon type="upload" />
             <span className="nav-text">nav 3</span>
           </Menu.Item>
-          <Menu.Item key="/Page4">
+          <Menu.Item key="/page4">
             <Icon type="bar-chart" />
             <span className="nav-text">nav 4</span>
           </Menu.Item>
-          <Menu.Item key="/Page5">
+          <Menu.Item key="/page5">
             <Icon type="cloud-o" />
             <span className="nav-text">nav 5</span>
           </Menu.Item>
-          <Menu.Item key="/Page6">
+          <Menu.Item key="/page6">
             <Icon type="appstore-o" />
             <span className="nav-text">nav 6</span>
           </Menu.Item>
-          <Menu.Item key="/Page7">
+          <Menu.Item key="/page7">
             <Icon type="team" />
             <span className="nav-text">nav 7</span>
           </Menu.Item>
-          <Menu.Item key="/Page8">
+          <Menu.Item key="/page8">
             <Icon type="shop" />
             <span className="nav-text">nav 8</span>
           </Menu.Item>
