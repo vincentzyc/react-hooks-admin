eda78aad793eb903d8bc1b1cbd996081654f5402
Vincentyun <vincentzyc@qq.com>
2019-05-09 10:47:36 +0800

    优化sidebar

--- src/layout/siderbar.jsx
+++ src/layout/siderbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import { withRouter } from "react-router-dom";
+import sidebarList from "./sidebar.js"
 
 const {Sider} = Layout;
 
@@ -21,42 +22,12 @@ function Siderbar(props) {
           defaultSelectedKeys={[props.location.pathname]}
           selectedKeys={[props.location.pathname]}
         >
-          <Menu.Item key="/">
-            <Icon type="user" />
-            <span className="nav-text">nav 1</span>
+          {sidebarList.map((item, index) => (
+          <Menu.Item key={item.key}>
+            <Icon type={item.icon} />
+            <span className="nav-text">{item.text}</span>
           </Menu.Item>
-          <Menu.Item key="/page2">
-            <Icon type="video-camera" />
-            <span className="nav-text">nav 2</span>
-          </Menu.Item>
-          <Menu.Item key="/page3">
-            <Icon type="upload" />
-            <span className="nav-text">nav 3</span>
-          </Menu.Item>
-          <Menu.Item key="/page4">
-            <Icon type="bar-chart" />
-            <span className="nav-text">nav 4</span>
-          </Menu.Item>
-          <Menu.Item key="/page5">
-            <Icon type="cloud-o" />
-            <span className="nav-text">nav 5</span>
-          </Menu.Item>
-          <Menu.Item key="/page6">
-            <Icon type="appstore-o" />
-            <span className="nav-text">nav 6</span>
-          </Menu.Item>
-          <Menu.Item key="/page7">
-            <Icon type="team" />
-            <span className="nav-text">nav 7</span>
-          </Menu.Item>
-          <Menu.Item key="/page8">
-            <Icon type="shop" />
-            <span className="nav-text">nav 8</span>
-          </Menu.Item>
-          <Menu.Item key="/todos">
-            <Icon type="hdd" />
-            <span className="nav-text">todos</span>
-          </Menu.Item>
+          ))}
         </Menu>
       </Sider>
   )
