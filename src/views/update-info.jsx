7b28810aa702de3e635816a25f0540640d2c689c
Vincentyun <vincentzyc@qq.com>
2020-07-17 09:15:30 +0800

    antd update

--- src/views/update-info.jsx
+++ src/views/update-info.jsx
@@ -1,72 +1,66 @@
 import React, { useState } from 'react';
+import { Form, Input, Button, Modal } from 'antd';
+import { LockOutlined } from '@ant-design/icons'
 
-import { Form, Icon, Input, Button, Modal } from 'antd';
-
-const updateInfo = props => {
-  const handleSubmit = e => {
-    e.preventDefault();
-    props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-        if (newPasswordError !== confirmPasswordError) {
-          // new Password and confirm Password are different
-          return Modal.warning({
-            title: 'Tip',
-            content: 'new Password and confirm Password are different',
-          });
-        }
-        Modal.success({
-          title: 'Tip',
-          content: 'Modified success',
-        });
-      }
+const UpdateInfo = () => {
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+    if (newPassword !== confirmPassword) {
+      // new Password and confirm Password are different
+      return Modal.warning({
+        title: 'Tip',
+        content: 'new Password and confirm Password are different',
+      });
+    }
+    Modal.success({
+      title: 'Tip',
+      content: 'Modified success',
     });
   };
-  const { getFieldDecorator } = props.form;
 
-  const [passwordError, setPasswordError] = useState('');
-  const [newPasswordError, setNewPasswordError] = useState('');
-  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   return (
-    <Form onSubmit={handleSubmit} style={{ width: '300px', margin: "50px auto" }}>
-      <Form.Item>
-        {getFieldDecorator('password', {
-          rules: [{ required: true, message: 'Please input your Password!' }],
-        })(
-          <Input
-            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            type="password"
-            placeholder="Password"
-            onChange={e => { setPasswordError(e.target.value) }}
-          />,
-        )}
+    <Form
+      onFinish={onFinish}
+      style={{ width: '300px', margin: "50px auto" }}>
+      <Form.Item
+        label="原密码"
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}>
+        <Input
+          prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          type="password"
+          placeholder="Password"
+          onChange={e => { setPassword(e.target.value) }}
+        />
       </Form.Item>
 
-      <Form.Item>
-        {getFieldDecorator('newPassword', {
-          rules: [{ required: true, message: 'Please input your New Password!' }],
-        })(
-          <Input
-            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            type="password"
-            placeholder="New Password"
-            onChange={e => { setNewPasswordError(e.target.value) }}
-          />,
-        )}
+      <Form.Item
+        label="新密码"
+        name="newPassword"
+        rules={[{ required: true, message: 'Please input your New Password!' }]}>
+        <Input
+          prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          type="password"
+          placeholder="New Password"
+          onChange={e => { setNewPassword(e.target.value) }}
+        />
       </Form.Item>
 
-      <Form.Item>
-        {getFieldDecorator('confirmPassword', {
-          rules: [{ required: true, message: 'Please confirm your New Password!' }],
-        })(
-          <Input
-            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            type="password"
-            placeholder="Confirm Password"
-            onChange={e => { setConfirmPasswordError(e.target.value) }}
-          />,
-        )}
+      <Form.Item
+        label="确认密码"
+        name="confirmPassword"
+        rules={[{ required: true, message: 'Please confirm your New Password!' }]}>
+
+        <Input
+          prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          type="password"
+          placeholder="Confirm Password"
+          onChange={e => { setConfirmPassword(e.target.value) }}
+        />
       </Form.Item>
 
       <Form.Item className="text-center">
@@ -75,14 +69,12 @@ const updateInfo = props => {
         </Button>
       </Form.Item>
       <Form.Item>
-        <p>passwordError:{passwordError}</p>
-        <p>newPasswordError:{newPasswordError}</p>
-        <p>confirmPasswordError:{confirmPasswordError}</p>
+        <p>password:{password}</p>
+        <p>newPassword:{newPassword}</p>
+        <p>confirmPassword:{confirmPassword}</p>
       </Form.Item>
     </Form>
   );
 }
 
-const WrappedUpdateInfo = Form.create({ name: 'update_info' })(updateInfo);
-
-export default WrappedUpdateInfo;
+export default UpdateInfo;
