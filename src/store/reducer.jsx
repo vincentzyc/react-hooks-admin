49649fb164f3ed7a14dafc7adfcf36724daea1e0
Vincentyun <vincentzyc@qq.com>
2020-07-27 14:10:53 +0800

    代码优化

--- src/store/reducer.jsx
+++ src/store/reducer.jsx
@@ -22,26 +22,7 @@ const reducers = {
       default:
         return state;
     }
-  },
-  setPage3List(state, data) {
-    switch (data.type) {
-      case "INIT_DATA":
-        if (!Array.isArray(data.payload)) {
-          return state;
-        }
-        return {
-          ...state,
-          page3List: data.payload
-        };
-      case "COMPLETE":
-        return {
-          ...state,
-          page3List: state.page3List.filter((item, i) => i !== data.payload)
-        };
-      default:
-        return state;
-    }
-  },
+  }
 }
 
 export default function reducer(state, action) {
