320658480c39b0313c308394b89db265dfbf5e4a
Vincentyun <vincentzyc@qq.com>
2020-07-27 10:37:51 +0800

    reducer setPage3List

--- src/store/reducer.jsx
+++ src/store/reducer.jsx
@@ -22,7 +22,26 @@ const reducers = {
       default:
         return state;
     }
-  }
+  },
+  setPage3List(state, data) {
+    switch (data.type) {
+      case "INIT_DATA":
+        if (!Array.isArray(data.payload)) {
+          return state;
+        }
+        return {
+          ...state,
+          page3List: data.payload
+        };
+      case "COMPLETE":
+        return {
+          ...state,
+          page3List: state.page3List.filter((item, i) => i !== data.payload)
+        };
+      default:
+        return state;
+    }
+  },
 }
 
 export default function reducer(state, action) {
